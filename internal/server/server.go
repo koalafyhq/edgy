75861e1b3af772fc48bd7340f784cc22b6c9a975
Fariz Rizaldy <farizrizaldy@icloud.com>
2020-05-31 17:50:05 +0700

    refactor: use hlog

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,11 +3,15 @@ package server
 import (
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/justinas/alice"
 	"github.com/koalafy/edgy/http/headers"
 	"github.com/koalafy/edgy/internal/cachemanager"
-	"github.com/koalafy/edgy/internal/logger"
 	"github.com/koalafy/edgy/internal/proxy"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/hlog"
+	"github.com/rs/zerolog/log"
 )
 
 func healthcheck(w http.ResponseWriter, r *http.Request) {
@@ -16,23 +20,48 @@ func healthcheck(w http.ResponseWriter, r *http.Request) {
 
 // New is
 func New(cacheMananger *cachemanager.CacheManager) *http.ServeMux {
+	log := zerolog.New(os.Stdout).With().
+		Timestamp().
+		Logger()
+
+	via := os.Getenv("EDGY_REGION")
 	reqIDKey := "x-edgy-req-id"
 	serverName := "edgy"
 
-	router := http.DefaultServeMux
-	proxy := proxy.New(cacheMananger)
+	c := alice.New()
+	c = c.Append(hlog.NewHandler(log))
+	c = c.Append(hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
+		hlog.FromRequest(r).Info().
+			Str("method", r.Method).
+			Str("url", r.URL.String()).
+			Str("host", r.Host).
+			Str("via", via).
+			Int("status", status).
+			Int("size", size).
+			Dur("duration", duration).
+			Msg("")
+	}))
 
-	via := os.Getenv("EDGY_REGION")
+	c = c.Append(hlog.RequestIDHandler("req_id", reqIDKey))
+	c = c.Append(headers.PoweredBy(serverName, via))
+
+	h := c.Then(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		proxy := proxy.New(cacheMananger)
+
+		proxy.ServeHTTP(w, r)
+	}))
+
+	router := http.DefaultServeMux
 
 	router.HandleFunc("/__healthcheckz", healthcheck)
-	router.Handle("/", headers.RequestID(reqIDKey, headers.PoweredBy(serverName, via, proxy)))
+	router.Handle("/", h)
 
 	return router
 }
 
 // Run is
 func Run(app *http.ServeMux) error {
-	logger.Debug("Server booted")
+	log.Debug().Msg("Server booted")
 
-	return http.ListenAndServeTLS(":3000", "certs/public.crt", "certs/private.key", app)
+	return http.ListenAndServe(":3000", app)
 }
